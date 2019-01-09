9113c35ea70b23f620e3921c3a8a50302ce55735
Julio García Pérez <juliojose.garcia@11paths.com>
2019-01-09 20:52:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const PORT = process.env.PORT || 3000;
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const usersRoutes = require('./routes/users.routes')
+
+const app = express();
+
+require('./config/db.config');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/public'));
+
+app.use('/users', usersRoutes);
+
+app.listen(PORT, () => {
+  console.info(`App listen at ${PORT} port`);
+});
+
---
+++ routes/users.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users.controller');
+
+router.get('/', usersController.list)
+router.get('/create', usersController.create)
+router.post('/create', usersController.doCreate)
+router.get('/:id', usersController.get)
+router.post('/:id/delete', usersController.delete)
+
+module.exports = router;
